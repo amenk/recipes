0ad30821143e969b63c85a9a76efcf9f507dd52b
Alexander Menk <a.menk@imi.de>
2022-05-24 16:14:11 +0200

    newline

--- shopware/storefront/6.4/bin/build-storefront.sh
+++ shopware/storefront/6.4/bin/build-storefront.sh
@@ -4,6 +4,8 @@ CWD="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 
 set -euo pipefail
 
+#new stuff
+
 export PROJECT_ROOT="${PROJECT_ROOT:-"$(dirname "$CWD")"}"
 STOREFRONT_ROOT="${STOREFRONT_ROOT:-"${PROJECT_ROOT}/vendor/shopware/storefront"}"
 
